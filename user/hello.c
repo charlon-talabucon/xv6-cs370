50d1858d5d64436e7ea3f03130fe88fe2e089ec1
CHARLON TALABUCON <talabuco@unlv.nevada.edu>
2026-02-08 23:32:03 -0800

    My solution for project 1 on xv6

---
+++ user/hello.c
@@ -0,0 +1,11 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main()
+{
+    printf("Hello world, this is Charlon Talabucon.\n");
+    printf("I am in CS370, spring 2026, Section 1002");
+
+    exit(0);
+}
